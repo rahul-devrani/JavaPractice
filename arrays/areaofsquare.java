3cf98a87dfec602f1b3433b004660ee279319e98
Rahul Devrani <142100974+rahul-devrani@users.noreply.github.com>
2023-09-05 19:48:59 +0530

    program to find area of square

---
+++ arrays/areaofsquare.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class areaofsquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter side of square : ");
+        int s= sc.nextInt();
+        System.out.println("Area of Square is "+ s*s);
+        
+    }
+    
+}
